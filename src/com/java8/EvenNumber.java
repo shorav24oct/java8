9697d5291a8d97a7f5ff859f8fd48f8b5f7701ba
shoravs <sharmasaurav142@gmail.com>
2025-01-16 19:24:28 +0530

    print even numbers

--- src/com/java8/EvenNumber.java
+++ src/com/java8/EvenNumber.java
@@ -2,10 +2,18 @@ package com.java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EvenNumber {
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(10,15,8,49,25,98,32);
+        List<Integer> numbers = Arrays.asList(10, 15, 8, 49, 25, 98, 32);
+
+        List<Integer> collected = numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(collected);
+
     }
 }
