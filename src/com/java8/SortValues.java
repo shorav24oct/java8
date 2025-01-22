9680447d9646cca0856703615629501e70524a81
shoravs <sharmasaurav142@gmail.com>
2025-01-22 10:47:29 +0530

    Sort Values from list of Integers

---
+++ src/com/java8/SortValues.java
@@ -0,0 +1,13 @@
+package com.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SortValues {
+    public static void main(String[] args) {
+        List<Integer> myList = Arrays.asList(10, 15, 8, 49, 25, 98, 98, 32, 15);
+
+
+
+    }
+}
