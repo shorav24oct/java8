958102f8a95229dcf2719f4762ddfb93df4d9eb1
shoravs <sharmasaurav142@gmail.com>
2025-01-22 16:04:58 +0530

    add stream methods for sorting

--- src/com/java8/SortValues.java
+++ src/com/java8/SortValues.java
@@ -7,7 +7,7 @@ public class SortValues {
     public static void main(String[] args) {
         List<Integer> myList = Arrays.asList(10, 15, 8, 49, 25, 98, 98, 32, 15);
 
-
+        myList.stream().sorted().forEach(System.out::println);
 
     }
 }
