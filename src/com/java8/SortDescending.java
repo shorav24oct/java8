cc8bbf86fd05a80d77f9cbba875074c967a2f362
shoravs <sharmasaurav142@gmail.com>
2025-01-24 19:43:18 +0530

    Sort Descending

--- src/com/java8/SortDescending.java
+++ src/com/java8/SortDescending.java
@@ -4,11 +4,10 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public class SortValues {
+public class SortDescending {
     public static void main(String[] args) {
         List<Integer> myList = Arrays.asList(10, 15, 8, 49, 25, 98, 98, 32, 15);
 
-        myList.stream().sorted().forEach(System.out::println);
-
+        myList.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
     }
 }
